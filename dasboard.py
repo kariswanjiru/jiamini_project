507a9e4ad5be56801483e46ec03622df12382acc
Wanjiru Kariuki <42275208+kariswanjiru@users.noreply.github.com>
2022-04-28 18:08:39 +0300

    Add files via upload

---
+++ dasboard.py
@@ -0,0 +1,46 @@
+import dash
+import dash_html_components as html
+import plotly.graph_objects as go
+import dash_core_components as dcc
+import plotly.express as px
+from dash.dependencies import Input, Output
+import pandas as pd
+
+data = pd.read_csv('pos_sales.csv')
+app = dash.Dash()
+
+
+
+
+app.layout = html.Div(id = 'parent', children = [
+    html.H1(id = 'H1', children = 'BI_dasboard', style = {'textAlign':'center',\
+                                            'marginTop':40,'marginBottom':40}),
+
+        dcc.Dropdown( id = 'dropdown',
+        options = [
+            {'label':'Month', 'value':'W' },
+            {'label': 'daily', 'value':'D'},
+            ],
+        value = 'month'),
+        dcc.Graph(id = 'bar_plot')
+    ])
+    
+    
+@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+              [Input(component_id='dropdown', component_property= 'value')])
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    fig = go.Figure([go.Scatter(x = data['month'], y = data['{}'.format(dropdown_value)],\
+                     line = dict(color = 'firebrick', width = 4))
+                     ])
+    
+    fig.update_layout(title = 'Stock prices over time',
+                      xaxis_title = 'Dates',
+                      yaxis_title = 'Prices'
+                      )
+    return fig  
+
+
+
+if __name__ == '__main__': 
+    app.run_server()
