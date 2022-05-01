c65f91903a5f3b67a64a4ac2b669a2370ede608f
Wanjiru Kariuki <42275208+kariswanjiru@users.noreply.github.com>
2022-05-01 20:46:58 +0300

    Update dasboard.py

--- dasboard.py
+++ dasboard.py
@@ -1,7 +1,7 @@
 import dash
-import dash_html_components as html
-import plotly.graph_objects as go
-import dash_core_components as dcc
+from dash import html
+
+from dash import dcc
 import plotly.express as px
 from dash.dependencies import Input, Output
 import pandas as pd
